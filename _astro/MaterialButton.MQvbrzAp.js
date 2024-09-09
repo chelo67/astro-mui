8b7b57f7df2f19ebcdc84ffbf926d3af39c61874
marcelo <marceloacampora@gmail.com>
2024-09-09 17:06:02 -0300

    Updates

--- _astro/MaterialButton.MQvbrzAp.js
+++
@@ -1,1 +0,0 @@
-import{j as t,B as n}from"./Button.B8Zd-r2J.js";import"./index.5GATsKCd.js";const o=()=>typeof window>"u"?null:t.jsx(n,{variant:"contained",children:"Hello, Material UI!"});export{o as default};

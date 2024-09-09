2597cb9ac2cf6f33b9fb4d1f6d3c49733e695e22
marcelo <marceloacampora@gmail.com>
2024-09-09 14:09:12 -0300

    1

--- src/components/SpeedDialComponent.jsx
+++ src/components/SpeedDialComponent.jsx
@@ -1,6 +1,5 @@
 // src/components/SpeedDialComponent.jsx
 
-import * as React from 'react';
 import Box from '@mui/material/Box';
 import SpeedDial from '@mui/material/SpeedDial';
 import SpeedDialIcon from '@mui/material/SpeedDialIcon';
